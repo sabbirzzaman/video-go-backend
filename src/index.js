0d164bc65d91c02295a2f7ee3caf2ae9add11034
sabbirzzaman <sabbirzzaman7@gmail.com>
2025-07-08 21:06:25 +0600

    basic middleware setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-import mongoose from "mongoose";
+import app from "./app.js";
+import connectDB from "./database/index.js";
 
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("Connection error: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running at port: ", process.env.PORT || 8000);
+    });
+  })
+  .catch((err) => {
+    console.error("Database Connection Error: ", err);
+  });
